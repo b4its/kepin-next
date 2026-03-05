d9e110c14768cd455f99c61a7f4a9b905da9e3eb
illnavt <127709923+illnavt@users.noreply.github.com>
2026-03-05 13:54:47 +0800

    Delete prisma directory

--- prisma/migrations/20260212070810_add_feature/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Feature" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-
-    CONSTRAINT "Feature_pkey" PRIMARY KEY ("id")
-);
