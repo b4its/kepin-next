aa2d4247327c00da9c487e559ae4b7cc730b1b97
illnavt <Zahariansyah0@gmail.com>
2026-03-05 14:02:40 +0800

    initial commit

---
+++ prisma/migrations/20260212070810_add_feature/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Feature" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Feature_pkey" PRIMARY KEY ("id")
+);
